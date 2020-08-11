816130f830665a26b3c69de7b2352f0c6792dbfc
BUCTCS207 <67367452+BUCTCS207@users.noreply.github.com>
2020-08-11 18:35:45 +0800

    Unity3D开发的打砖块游戏

---
+++ BreakTheBricksProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 3;//控制相机移动速度
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {//每帧Update60次
+        //按下左（下）返回-1， 按下右（上）返回1
+        float h = Input.GetAxis("Horizontal");//可以监测水平方向键（键盘的）的按下
+        float v = Input.GetAxis("Vertical");//可以监测竖直方向键（键盘的）的按下
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * 3);//Time.deltaTime 使得速度变为1
+        //h在x轴上，v在y轴上
+	}
+}
---
+++ BreakTheBricksProject/Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float speed = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = b.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;//方向乘以速度
+        }
+	}
+}
